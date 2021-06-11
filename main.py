1edc000218cbbaebd188f546e950736d9960329e
Nixon Pinargote <poposca@gmail.com>
2021-06-10 20:50:15 -0500

    Clear color change; Added function to draw boardlines

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 import pyglet
+from pyglet import shapes
 
-window = pyglet.window.Window()
+background = pyglet.graphics.OrderedGroup(0)
+foreground = pyglet.graphics.OrderedGroup(1)
 
-label = pyglet.text.Label('Hello, world',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
+
+def backrgound_draw():
+    mybatch = pyglet.graphics.Batch()
+
+    width = 2    
+    color = (200, 200, 200)
+
+    vertical_lines = []
+    horizontal_lines = []
+
+    for i in range(11):
+        # Vertical Line
+        vco_x1 = 80 + (64*i)
+        vco_y1 = 580
+        vco_x2 = 80 + (64*i)
+        vco_y2 = 60
+        hco_x1 = 80
+        hco_y1 = 60 + (52*i)
+        hco_x2 = 720
+        hco_y2 = 60 + (52*i)
+         
+        # creating a line
+        horizontal_lines.append(shapes.Line(vco_x1, vco_y1, vco_x2, vco_y2, width, color = color, batch = mybatch, group=background)) 
+        horizontal_lines[-1].opacity = 222
+
+        horizontal_lines.append(shapes.Line(hco_x1, hco_y1, hco_x2, hco_y2, width, color = color, batch = mybatch, group=background)) 
+        horizontal_lines[-1].opacity = 222
+     
+    mybatch.draw()
+
+
+window = pyglet.window.Window(800, 600, "10x10 Tic Tac Toe")
+pyglet.gl.glClearColor(0.1,0.1,0.14,1) 
 
 @window.event
 def on_draw():
     window.clear()
-    label.draw()
+    backrgound_draw()
 
 pyglet.app.run()
